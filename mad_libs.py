6ccc41868ff162e3ff27221222277478ecd8e455
System Administrator <root@TP-Kapor-MacBook.local>
2015-10-05 20:53:26 -0700

    Practicing git

---
+++ mad_libs.py
@@ -0,0 +1,20 @@
+print "Welcome to Tiffany's Mad Lib application.  I will ask you for a series of words.  Please enter one word for each word type, and enjoy your Mad Lib experience!"
+name = raw_input("What's your name?")
+verb1 = raw_input("Type a verb. ")
+adjective1 = raw_input("Type an adjective (descriptive word). ")
+plural_noun1 = raw_input("Type a plural noun. ")
+adjective2 = raw_input("Type an adjective. ")
+ing_verb1 = raw_input("Type a verb ending in -ing. ")
+verb2 = raw_input("Type another verb. ")
+num1 = raw_input("Type a number. ")
+adjective3 = raw_input("Type an adjective. ")
+plural_noun2 = raw_input("Type a plural noun. ")
+plural_noun3 = raw_input("Type another plural noun. ")
+plural_noun4 = raw_input("Type another plural noun. ")
+plural_relative = raw_input("Type a plural relative (i.e. parents, sisters, etc.) ")
+adjective4 = raw_input("Type an adjective. ")
+adjective5 = raw_input("Type another adjective. ")
+plural_noun4 = raw_input("Type a plural noun. ")
+print "Here's your Mad Lib!"
+print ""
+print "Come " + verb1 + " at " + name + "'s Country Store, where you`ll receive " + adjective1 + " discounts on all of your favorite brand name " + plural_noun1 + "." + " Our " + adjective2 + " and " + ing_verb1 + " associates are here to " + verb2 + " you. Here, you will find " + adjective3 + " prices on the " + plural_noun2 + " you need: " + plural_noun3 + " for the moms, " + plural_noun4 + " for the kids, and all the latest electronics for the " + plural_relative + ". So come on down to your " + adjective4 + ", " + adjective5 + " " + name + "'s Country Store where the " + plural_noun4 + " come first."
